9858deb3c3581cc280ffd1645d4683aeacf04485
pkondeti <padma.kondeti@gmail.com>
2016-07-29 00:22:19 -0400

    working version for selection  & purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,81 +14,89 @@ var connection= mysql.createConnection({
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
-	getAllProducts();
-	
+	getAllProducts();	
 });
 var getAllProducts = function(){
 	var sqlQuery = "SELECT * FROM products";
-	//selectDBData(connection, sqlQuery);
-
 	connection.query(sqlQuery, function(err, res){
 		if ( res.length ) {
 			var table = new Table({
-				head: ['itemID', 'productName', 'departmentName', 'Price', 'stockQuantity'],
-				colWidths: [10, 20, 20, 10, 20]
+				head: ['ItemID', 'ProductName', 'Price'],
+				colWidths: [10, 20, 20]
 			});
 			for (var i = 0; i < res.length; i++){
 				table.push(
-				[res[i].itemID, res[i].productName, res[i].departmentName, res[i].price, res[i].stockQuantity]);
+				[res[i].itemID, res[i].productName, res[i].price]);
 			};
 			console.log(table.toString());	
-			getProdctDetails();
-		} else {
-			console.log("Sorry Insufficient quantity");
-		}
+			promptCustomer();
+		};
 		
 	});
 
 };
-// displaying data by acceptingproduct Id and quantity from the user
-var getProdctDetails = function(){
-	prompt("Please Enter the required product ID: ", function(val){
-		var productID = val;
-		prompt("Please Enter the Quantity: ", function(val){
-			var quantity = val;
-			var sqlQuery = 'SELECT * FROM products WHERE itemID = ' + productID + ' AND ' +  quantity +
-' < stockQuantity' ;
-			
-		connection.query(sqlQuery, function(err, res){
-		if ( res.length ) {
-			var table = new Table({
-				head: ['itemID', 'productName', 'departmentName', 'Price', 'stockQuantity'],
-				colWidths: [10, 20, 20, 10, 20]
-			});
-			for (var i = 0; i < res.length; i++){
-				table.push(
-				[res[i].itemID, res[i].productName, res[i].departmentName, res[i].price, res[i].stockQuantity]);
-			};
-			console.log(table.toString());	
-		} else {
-			console.log("Sorry Insufficient quantity");
-		}
+//get response from the customer
+var promptCustomer = function(){
+	inquirer.prompt([
+	{
+		type: 'input',
+		name:'choice',
+		message:'What would you like to purchase? [Quit with Q]'
 		
-	});
-		}, function(err){
-			console.error("error reading Quantity:  " + err);
-		});
-	}, function(err){
-		console.error("error reading product #:  " + err);
-	});
-};
+	},
+	{	type: 'input',
+		name:'quantity',
+		message:'How many do you like to purchase? [Quit with Q]'	
+	}
 
-var selectDBData = function(connection, sqlQuery){
-	connection.query(sqlQuery, function(err, res){
-		console.log('response from selectDB function');
-		if ( res.length ) {
-			var table = new Table({
-				head: ['itemID', 'productName', 'departmentName', 'Price', 'stockQuantity'],
-				colWidths: [10, 20, 20, 10, 20]
-			});
-			for (var i = 0; i < res.length; i++){
-				table.push(
-				[res[i].itemID, res[i].productName, res[i].departmentName, res[i].price, res[i].stockQuantity]);
+	]).then(function(val){
+		if (isNaN(val.choice) || isNaN(val.quantity) ){
+			 
+			if (isNaN(val.choice)) {
+				console.log(val.choice);
+				// Pass the return value in the done callback
+				console.log('You need to provide a number');
 			};
-			console.log(table.toString());	
+			if (isNaN(val.quantity)) {			
+				console.log('You need to provide a number');
+			}
+			promptCustomer();
+		} else {
+			getProdctDetails(val.choice, val.quantity);
+			return;
+		}
+	});
+}
+// displaying data by accepting product Id and quantity from the user
+var getProdctDetails = function(productID, quantity){
+	var sqlQuery = 'SELECT * FROM Products WHERE ItemID= '+ productID ;
+	connection.query(sqlQuery, function(err, res){
+		if (err) throw err;
+		if ( res.length ) {
+			if (res[0].stockQuantity  >= quantity){
+				var sqlQuery = 'UPDATE products SET stockQuantity = stockQuantity - ' + quantity + ' WHERE ItemID = ' + productID ;
+				connection.query(sqlQuery, function(err, res){
+					if (err) throw err;
+					var sqlQuery = 'SELECT * FROM Products WHERE ItemID= '+ productID ;
+					connection.query(sqlQuery, function(err, res){
+						if (err) throw err;
+						var table = new Table({
+							head: ['itemID', 'productName', 'Price', 'Quantity', 'Total Ammount'],
+							colWidths: [10, 20, 20, 10, 20]
+						});
+						for (var i = 0; i < res.length; i++){
+							table.push(
+							[res[i].itemID, res[i].productName, res[i].price, quantity, res[i].price * quantity	]);
+						};
+						console.log('\nHERE ARE THE PURCHASE DETAILS');
+						console.log(table.toString());	
+						connection.end();
+					});	
+				})
 		} else {
 			console.log("Sorry Insufficient quantity");
-		}
-		return;
-	});
+			connection.end();
+		};
+	}
+});
 };
